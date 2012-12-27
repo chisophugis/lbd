da44c7a42fa0aa4d4d3c31d93dfba561612f6e79
Jonathan <gamma_chen@yahoo.com.tw>
2012-12-27 16:42:02 +0800

    correct ch3.cpp error

--- LLVMBackendTutorialExampleCode/InputFiles/ch3.cpp
+++ LLVMBackendTutorialExampleCode/InputFiles/ch3.cpp
@@ -2,7 +2,7 @@
 // /usr/local/llvm/3.1.test/cpu0/1/cmake_debug_build/bin/llc -march=cpu0 -relocation-model=pic -filetype=asm ch3.bc -o ch3.cpu0.s
 // /usr/local/llvm/3.1.test/cpu0/1/cmake_debug_build/bin/llc -march=cpu0 -relocation-model=pic -filetype=obj ch3.bc -o ch3.cpu0.o
 
-/Users/Jonathan/llvm/3.1.test/cpu0/1/cmake_debug_build/bin/Debug/llc -march=cpu0 -relocation-model=pic -filetype=asm ch3.bc -o ch3.cpu0.s
+// /Users/Jonathan/llvm/3.1.test/cpu0/1/cmake_debug_build/bin/Debug/llc -march=cpu0 -relocation-model=pic -filetype=asm ch3.bc -o ch3.cpu0.s
 int main()
 {
 	return 0;
