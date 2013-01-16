ea3353f378f61d3c2eb0f544f8e03fd56c726c42
Jonathan <gamma_chen@yahoo.com.tw>
2013-01-16 18:18:56 +0800

    Update source_ExampleCode

--- source_ExampleCode/InputFiles/ch4_6.cpp
+++ source_ExampleCode/InputFiles/ch4_6.cpp
@@ -1,4 +1,5 @@
 // clang -c ch4_6.cpp -emit-llvm -o ch4_6.bc
+// /Users/Jonathan/llvm/test/cmake_debug_build/bin/Debug/llc -march=cpu0 -relocation-model=pic -filetype=asm ch4_6.bc -o ch4_6.cpu0.s
 // /Users/Jonathan/llvm/test/cmake_debug_build/bin/Debug/llc -march=cpu0 -view-isel-dags -view-sched-dags -relocation-model=pic -filetype=asm ch4_6.bc -o ch4_6.cpu0.s
 
 int main()
